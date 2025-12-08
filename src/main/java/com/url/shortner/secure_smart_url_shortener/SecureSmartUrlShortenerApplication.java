fb474eb3d82d2fa714e45913ff5dab479c3cf7c1
FaeemazazB <faeemazaz.bhanej@ayasya.com>
2025-12-08 23:34:52 +0530

    test start

---
+++ src/main/java/com/url/shortner/secure_smart_url_shortener/SecureSmartUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortner.secure_smart_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSmartUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureSmartUrlShortenerApplication.class, args);
+	}
+
+}
